5be9ebbcb6b3cf214eb36bc77685661357317c9b
ANJAL HUSSAN <anjal@ibm.com>
2022-06-23 17:09:56 -0400

    remove port number

--- src/main/java/com/ibm/viewer/Viewer.java
+++ src/main/java/com/ibm/viewer/Viewer.java
@@ -3,7 +3,6 @@ import static spark.Spark.*;
 
 public class Viewer {
     public static void main(String[] args) {
-        port(8080);
         get("/", (req, res) -> "Hello World! ✨ It's fun to use sparkly emojis in code!");
     }
 
